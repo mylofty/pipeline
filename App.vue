f86ccffc1b00340184dfa523e0856d615ded279a
zhiminding <zhiminding@tencent.com>
2025-10-08 17:04:48 +0800

    delete test code

--- App.vue
+++ App.vue
@@ -7,7 +7,6 @@
 <script>
 	import database from '@/utils/database.js';
 	import databaseTest from '@/utils/databaseTest.js';
-	import { pipeTypeEntityServiceTest } from '@/services/pipeTypeEntityService.test.js';
 	
 	export default {
 		async onLaunch() {
@@ -28,7 +27,6 @@
 						setTimeout(async () => {
 							try {
 								await databaseTest.runAllTests();
-								await pipeTypeEntityServiceTest.runFullTest();
 							} catch (testError) {
 								console.error('数据库测试失败:', testError);
 							}
